6a81c0e920104ead5d4be3fee2aefaaf88b95d86
viniciusVPC <viniciuspereira2206@gmail.com>
2024-04-17 22:52:21 -0300

    Novo layout de janela

--- src/main/java/VIEW/Main.java
+++ src/main/java/VIEW/Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static void main(String[] args) {
 
         Main main = new Main();
-        InsereLivroVIEW newFrame = new InsereLivroVIEW();
+        GeneralWindowVIEW newFrame = new GeneralWindowVIEW();
         newFrame.setVisible(true);
     }
 
